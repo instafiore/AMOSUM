31e1e84948ded1d8cbd30ba52511a960fbf71054
Salvatore Fiorentino <fiorentinosalvatore65@gmail.com>
2026-08-07 09:54:23 +0200

    before adding benchmarks

--- tests/profiling/pprof/pdf.bash
+++
@@ -1,4 +0,0 @@
-OUT=output.prof
-bin="/Users/instafiore/Workspace/AMOSUM/amosum/amoclingo/propagator_clingo_c/bin/./amosum_cpp"
-pprof --pdf $bin $OUT > ${OUT%.prof}.pdf 2>/dev/null
-open ${OUT%.prof}.pdf
